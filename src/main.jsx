381440c1ec275df4439fc1960dc775cc71086c77
Denni <dennipes1@gmail.com>
2024-06-08 15:06:30 +0700

    fixed route

--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import LoginPage from './Pages/LoginPage.jsx'
-import DashboardPage from './Pages/DashboardPage.jsx'
-import BiodataPage from './Pages/BiodataPage.jsx'
-import PengaturanPage from './Pages/PengaturanPage.jsx'
-import JadwalKuliahPage from './Pages/JadwalKuliahPage.jsx'
-import NotFoundPage from './Pages/NotFoundPage.jsx'
-import AkademikPage from './Pages/AkademikPage.jsx'
-import UseptPage from './Pages/UseptPage.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import LoginPage from './Pages/LoginPage.jsx';
+import DashboardPage from './Pages/DashboardPage.jsx';
+import BiodataPage from './Pages/BiodataPage.jsx';
+import PengaturanPage from './Pages/PengaturanPage.jsx';
+import JadwalKuliahPage from './Pages/JadwalKuliahPage.jsx';
+import NotFoundPage from './Pages/NotFoundPage.jsx';
+import AkademikPage from './Pages/AkademikPage.jsx';
+import UseptPage from './Pages/UseptPage.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/Login",
-    element: <LoginPage />
-  },
-  {
-    path: "/",
-    element: <DashboardPage />
-  },
-  {
-    path: "/Biodata",
-    element: <BiodataPage />
-  },
-  {
-    path: "/Pengaturan",
-    element: <PengaturanPage />
-  },
-  {
-    path: "/JadwalKuliah",
-    element: <JadwalKuliahPage />
-  },
-  {
-    path: "*",
-    element: <NotFoundPage />
-  },
-  {
-    path:"/Akademik",
-    element: <AkademikPage />
-  },
-  {
+// Membuat router
+const router = createBrowserRouter({
+  routes: [
+    { path: "/Login", element: <LoginPage /> },
+    { path: "/", element: <DashboardPage /> },
+    { path: "/Biodata", element: <BiodataPage /> },
+    { path: "/Pengaturan", element: <PengaturanPage /> },
+    { path: "/JadwalKuliah", element: <JadwalKuliahPage /> },
+    { path: "/Akademik", element: <AkademikPage /> },
+    { path: "/Usept", element: <UseptPage /> },
+    { path: "*", element: <NotFoundPage /> }
+  ]
+});
 
-    path: "/Usept",
-    element: <UseptPage />
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
